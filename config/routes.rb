c73cc7319301059ddb860ee6318f03a106103189
stanleycao <cxf1976@sohu.com>
2011-05-31 20:23:44 -0400

    Removed default Rails page 3

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ SampleApp::Application.routes.draw do
   match '/help', :to => 'pages#help' 
   root :to => "pages#home"
   
-  #get "pages/home"
-  #get "pages/contact"
-  #get "pages/about"
-  #get "pages/help"
+  get "pages/home"
+  get "pages/contact"
+  get "pages/about"
+  get "pages/help"
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
